141402dd55fe346c6264066fc41716082ab76410
Oskar Hýbl <oskar@hybl.cz>
2016-11-06 10:56:02 +0100

    Fixed move - replaced y coordinate with the correct z coord.

--- btCommandScripts/move.lua
+++ btCommandScripts/move.lua
@@ -10,27 +10,26 @@ function cmd.run(unitIds, parameter)
 	cmd.indexOfComma = string.find(parameter, ",")
 	
 	dx = parameter:sub(1, cmd.indexOfComma - 1)
-	dy = parameter:sub(cmd.indexOfComma + 1)
+	dz = parameter:sub(cmd.indexOfComma + 1)
 	
-	Spring.Echo("Lua MOVE command run, unitIds: " .. dump(unitIds) .. ", parameter: " .. parameter .. ", dx: " .. dx .. ", dy: " .. dy .. ", tick: "..cmd.n)
+	Spring.Echo("Lua MOVE command run, unitIds: " .. dump(unitIds) .. ", parameter: " .. parameter .. ", dx: " .. dx .. ", dz: " .. dz .. ", tick: "..cmd.n)
 	cmd.n = cmd.n + 1
 	done = true
-	x,y,z=0
-
+	
+	x,y,z = 0,0,0
 	for i = 1, #unitIds do
 		x, y, z = Spring.GetUnitPosition(unitIds[i])
 		
 		tarX = x + dx
-		tarY = y + dy
-		Spring.Echo("tar X - " .. tarX .. " tar Y - " .. tarY)
+		tarZ = z + dz
 		
 		if not cmd.targets[unitIds[i]] then
-			cmd.targets[unitIds[i]] = {tarX,tarY,z}
+			cmd.targets[unitIds[i]] = {tarX,y,tarZ}
 			Spring.GiveOrderToUnit(unitIds[i], CMD.MOVE, cmd.targets[unitIds[i]], {})  
 		end
 		
-		Spring.Echo("AtX: " .. x .. ", TargetX: " .. cmd.targets[unitIds[i]][1] .. " AtY: " .. y .. ", TargetY: " .. cmd.targets[unitIds[i]][2])
-		if math.abs(x - cmd.targets[unitIds[i]][1]) > 10 or math.abs(y - cmd.targets[unitIds[i]][2]) > 10 then
+		Spring.Echo("AtX: " .. x .. ", TargetX: " .. cmd.targets[unitIds[i]][1] .. " AtZ: " .. z .. ", TargetZ: " .. cmd.targets[unitIds[i]][2])
+		if math.abs(x - cmd.targets[unitIds[i]][1]) > 10 or math.abs(z - cmd.targets[unitIds[i]][3]) > 10 then
 			done = false
 		end
  	end
