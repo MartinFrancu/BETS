0c0608b9a8633775f95a8ce81dc09026cb6e0a1b
Michal Mojzik <michal@mojzik.net>
2017-01-07 10:37:06 +0100

    Actually working sensor environment setup, example of a sensor that calculates the center of the group and the maximal distance of a unit from that center.

--- BtEvaluator/SensorManager.lua
+++ BtEvaluator/SensorManager.lua
@@ -4,22 +4,28 @@
 WG.SensorManager = WG.SensorManager or (function()
 	local Utils = VFS.Include(LUAUI_DIRNAME .. "Widgets/BtUtils/root.lua", nil, VFS.RAW_FIRST)
 
+	local getGameFrame = Spring.GetGameFrame;
+	
 	local SENSOR_DIRNAME = LUAUI_DIRNAME .. "Widgets/BtSensors/"
 	local sensors = {}
 	local sensorEnvironmentMetatable = {
 		__metatable = false,
-		__index = System,
+		__index = {}
 	};
+	for k, v in pairs(widget) do
+		sensorEnvironmentMetatable.__index[k] = v
+	end
 	local managerForGroup = {}
 
 	local SensorManager = {}
 	function SensorManager.loadSensor(name)
-		local sensorCode = VFS.LoadFile(SENSOR_DIRNAME .. name .. ".lua")
+		local sensorCode = "--[[" .. name .. "]] " .. VFS.LoadFile(SENSOR_DIRNAME .. name .. ".lua")
 		local sensorFunction = assert(loadstring(sensorCode))
-		local function sensorConstructor(group)
+		local function sensorConstructor(groupSensorManager, group)
+			group.length = #group
 			local sensorEnvironment = setmetatable({
 				units = group,
-				Sensors = SensorManager.forGroup(group),
+				Sensors = groupSensorManager, --SensorManager.forGroup(group),
 			}, sensorEnvironmentMetatable)
 			setfenv(sensorFunction, sensorEnvironment)
 			local evaluator = sensorFunction()
@@ -28,12 +34,12 @@ WG.SensorManager = WG.SensorManager or (function()
 			if(sensorEnvironment.getInfo)then
 				info = sensorEnvironment.getInfo()
 			end
-			info.period = info.period or 60
+			info.period = info.period or 0
 			
 			local lastExecutionFrame = nil
 			local lastResult = nil
 			return function(...)
-				local currentFrame = Spring.GetGameFrame()
+				local currentFrame = getGameFrame()
 				if(lastExecutionFrame == nil or currentFrame - lastExecutionFrame >= info.period)then
 					lastResult = { evaluator(...) }
 					lastExecutionFrame = currentFrame
@@ -49,12 +55,12 @@ WG.SensorManager = WG.SensorManager or (function()
 		if(not manager)then
 			manager = setmetatable({}, {
 				__index = function(self, key)
-					sensorConstructor = sensors[key]
+					local sensorConstructor = sensors[key]
 					if(not sensorConstructor)then
 						sensorConstructor = SensorManager.loadSensor(key)
 						sensors[key] = sensorConstructor
 					end
-					local sensor = sensorConstructor(group);
+					local sensor = sensorConstructor(manager, group);
 					rawset(self, key, sensor)
 					return sensor
 				end,
---
+++ BtSensors/groupExtents.lua
@@ -0,0 +1,37 @@
+function getInfo()
+	return {
+		period = 60
+	}
+end
+
+local getPosition = Spring.GetUnitPosition;
+
+local function euclidianDistance(x, z)
+	return math.sqrt(x*x + z*z)
+end
+
+return function()
+	if(units.length == 0)then
+		return nil
+	end
+	
+	local centerX, _, centerZ = getPosition(units[1])
+	for i = 2, units.length do
+		local x, _, z = getPosition(units[i])
+		centerX = centerX + x
+		centerZ = centerZ + z
+	end
+	centerX = centerX / units.length
+	centerZ = centerZ / units.length
+
+	local maxDistance = 0
+	for i = 1, units.length do
+		local x, _, z = getPosition(units[i])
+		local distance = euclidianDistance(centerX - x, centerZ - z)
+		if(distance > maxDistance)then
+			maxDistance = distance
+		end
+	end
+	
+	return { center = { x = centerX, z = centerZ }, maxDistance = maxDistance }
+end
