f34af9fb6bc6952e65b405d717daa68d7632e5dc
Oskar Hýbl <oskar@hybl.cz>
2017-02-20 03:50:25 +0100

    Optimized version of move (one unit computes the whole path, others in the group follow its movement). Committing as a separate command for now as it hasn't been thoroughly tested yet.

---
+++ BtCommandScripts/moveOptim.lua
@@ -0,0 +1,190 @@
+function getInfo()
+	return {
+		onNoUnits = SUCCESS,
+		parameterDefs = {
+			{
+				name = "x",
+				variableType = "number",
+				componentType = "editBox",
+				defaultValue = "0",
+			},
+			{
+				name = "y",
+				variableType = "number",
+				componentType = "editBox",
+				defaultValue = "0",
+			}
+		}
+	}
+end
+
+local giveOrderToUnit = Spring.GiveOrderToUnit
+--local getUnitDir = Spring.GetUnitDirection
+local unitIsDead = Spring.GetUnitIsDead
+
+local SHORT_PATH_LEN = 100
+local SUBTARGET_TOLEARANCE_SQ = 30*30
+local TOLERANCE_SQ = 30*30
+
+local function wrapResVect(f, id)
+	local x, y, z = f(id)
+	return { x, y, z}
+end
+
+local function getUnitPos(unitID)
+  return wrapResVect(Spring.GetUnitPosition, unitID)
+end
+
+local function getUnitDir(unitID)
+	return wrapResVect(Spring.GetUnitDirection, unitID)
+end
+
+local function direction(from, to)
+	local dir = {to[1] - from[1], to[2] - from[2], to[3] - from[3]}
+	local len = math.sqrt(dir[1] * dir[1] + dir[2] * dir[2] + dir[3] * dir[3])
+	return {dir[1] / len, dir[2] / len, dir[3] / len }
+end
+
+local function dirsEqual(v1, v2)
+	local ratioX = v1[1] / v2[1]
+	local ratioZ = v1[3] / v2[3]
+	local tolerance = 0.05
+	return math.abs(ratioZ - ratioX) < tolerance
+end
+
+local function distanceSq(pos1, pos2)
+	local dist = 0
+	for i = 1,3 do
+		local d = pos1[i] - pos2[i]
+		dist = dist + d * d
+	end
+	return dist
+end
+
+local function UnitMoved(self, unitID, x, _, z)
+	local lastPos = self.lastPositions[unitID]
+	if not lastPos then
+		lastPos = {x = x, z = z}
+		self.lastPositions[unitID] = lastPos
+		Logger.log("move-command","unit:", unitID, "x: ", x ,", z: ", z)
+		return true
+	end
+	Logger.log("move-command", "unit:", unitID, " x: ", x ,", lastX: ", lastPos.x, ", z: ", z, ", lastZ: ", lastPos.z)
+	moved = x ~= lastPos.x or z ~= lastPos.z
+	self.lastPositions[unitID] = {x = x, z = z}
+	return moved
+end
+
+function New(self)
+	Logger.log("command", "Running New in move")
+	self.lastPositions = {}
+	self.subTargets = {}
+	self.finalTargets = {}
+end
+
+local function EnsureLeader(self, unitIds)
+	if self.leaderId and not unitIsDead(self.leaderId) and distanceSq(getUnitPos(self.leaderId), self.finalTargets[self.leaderId]) > TOLERANCE_SQ then
+		return self.leaderId
+	end
+	
+	self.leaderId = nil
+	for i = 1, #unitIds do
+		if not unitIsDead(unitIds[i]) then
+			self.leaderId = unitIds[i]
+			break
+		end
+	end
+	
+	Logger.log("move-command", "=== finalTargets: ", self.finalTargets)
+	local tar = self.finalTargets[self.leaderId]
+	Logger.log("move-command", "=== tar: ", tar)
+	giveOrderToUnit(self.leaderId, CMD.MOVE, tar, {})
+	return self.leaderId
+end
+
+local function InitTargetLocations(self, unitIds, parameter)
+	self.finalTargets = {}
+	for i = 1, #unitIds do
+		local id = unitIds[i]
+		local pos = getUnitPos(id)
+		Logger.log("move-command", "=== pos: ", pos)
+		local tarPos = { pos[1] + parameter.x, pos[2], pos[3] + parameter.y }
+		Logger.log("move-command", "=== tarPos: ", tarPos)
+		self.finalTargets[id] = tarPos
+	end
+end
+
+function Run(self, unitIds, parameter)
+	Logger.log("move-command", "Lua MOVE command run, unitIds: ", unitIds, ", parameter.x: " .. parameter.x .. ", parameter.y: " .. parameter.y)
+	
+	local firstTick = false
+	if not self.finalTargets then
+		InitTargetLocations(self, unitIds, parameter)
+		firstTick = true
+	end
+	
+	local leader = EnsureLeader(self, unitIds)
+	if not leader then
+		return FAILURE
+	end
+	
+	local leaderDir = getUnitDir(leader)
+	
+	local leaderDone = distanceSq(getUnitPos(leader), self.finalTargets[leader]) < TOLERANCE_SQ
+	Logger.log("move-command", "Leader done - ", leaderDone, " dist - ", distanceSq(getUnitPos(leader), self.finalTargets[leader]))
+	local done = leaderDone
+	
+	local issueFinalOrder = false
+	if leaderDone and not self.finalOrderIssued then
+		self.finalOrderIssued = true
+		issueFinalOrder = true
+	end
+	
+	for i=1,#unitIds do
+		local unitID = unitIds[i]
+		if unitID ~= leader then
+			local curPos = getUnitPos(unitID)
+			local curSubTar = self.subTargets[unitID]
+			local curDir = direction(getUnitPos(unitID), curSubTar or {0,0,0})
+			
+			if done and distanceSq(curPos, self.finalTargets[unitID]) > TOLERANCE_SQ then
+				done = false
+			end
+			
+			if leaderDone then
+				-- go to the target location when leader reaches the target location
+				if issueFinalOrder then
+					Logger.log("move-command", "=== Final order ", unitID)
+					giveOrderToUnit(unitID, CMD.MOVE, self.finalTargets[unitID], {})
+				end
+			elseif not curSubTar or distanceSq(curPos, curSubTar) < SUBTARGET_TOLEARANCE_SQ or not dirsEqual(leaderDir, curDir) then
+				-- otherwise move a small distance in the direction the leader is facing
+				
+				if firstTick then -- the leader needs to turn first, so send the unit in the direction of the target instead of following the leaders direction, which may be left over from the previous move command.
+					leaderDir = direction(curPos, self.finalTargets[unitID])
+				end
+				
+				curSubTar = {curPos[1] + leaderDir[1] * SHORT_PATH_LEN, curPos[2], curPos[3] + leaderDir[3] * SHORT_PATH_LEN}
+				self.subTargets[unitID] = curSubTar
+				giveOrderToUnit(unitID, CMD.MOVE, curSubTar, {})
+			end
+		end
+	end
+	if done then
+		return SUCCESS
+	--elseif noneMoved then
+	--	return FAILURE
+	else
+		return RUNNING
+	end
+end
+
+function Reset(self)
+	Logger.log("move-command", "Lua command reset")
+	self.leaderTarget = nil
+	self.lastPositions = {}
+	self.leaderId = nil
+	self.subTargets = {}
+	self.finalTargets = nil
+	self.finalOrderIssued = false
+end
