f0b53fca064bf189925d5d0df57c60625a0ebcfa
Michal Mojzík <michal@mojzik.net>
2017-03-10 22:10:48 +0100

    Blackboard visualisation now allows expanding tables to see their contents.

--- BtCreator/BtCreator.lua
+++ BtCreator/BtCreator.lua
@@ -266,12 +266,11 @@ end
 local blackboardWindowState
 local createRows, rowsMetatable
 do
-	local ROW_HEIGHT = 20
+	local TEXT_HEIGHT = 20
 	local metapairs = Utils.metapairs
-	local expandedVariablesMap = {} 
 	
 	local function makeCaption(k, v)
-		return tostring(k) .. " = " .. dump(v)
+		return tostring(k) .. " = " .. ((type(v) ~= "table" or (getmetatable(v) or {}).__tostring) and tostring(v) or "{...}")
 	end
 	local rowsPrototype = {}
 	function rowsPrototype:SetTable(t)
@@ -287,7 +286,7 @@ do
 		local top = 0
 		for i = 1, length do
 			local row = self[i]
-			if(not t[row.key])then
+			if(t[row.key] == nil)then
 				row.control:Dispose()
 				keyMap[row.key] = nil
 				offset = offset + 1
@@ -295,24 +294,80 @@ do
 				self[i - offset] = row
 				row.index = i - offset
 				row.control:SetPos(0, top)
-				row.control:SetCaption(makeCaption(row.key, t[row.key]))
+				local v = t[row.key]
+				row.currentValue = v
+				row.control.children[1]:SetCaption(makeCaption(row.key, v))
+				if(row.subrows)then
+					row.subrows:SetTable(v)
+				end
 				top = top + row.height
 			end
 		end
 		length = length - offset
 		for k, v in metapairs(t) do
 			if(not keyMap[k])then
-				local row = {
+				local row; row = {
 					key = k,
-					control = Chili.Label:New{
+					currentValue = v,
+					control = Chili.Control:New{
 						parent = self.wrapper,
 						x = 0,
 						y = top,
-						caption = makeCaption(k, v),
+						padding = {0,0,0,0},
+						width = '100%',
+						height = TEXT_HEIGHT*4,
+						children = { Chili.Label:New{
+							x = 0,
+							y = 0,
+							caption = makeCaption(k, v),
+							OnMouseUp = (type(v) == "table" and { function(control)
+								if(row.panel)then
+									row:Contract()
+								else
+									row:Expand()
+								end
+								self:Realign()
+								return control
+							end }) or nil
+						}, },
 					},
-					height = 20,
+					height = TEXT_HEIGHT,
+					Contract = function(row)
+						if(not row.panel)then return end
+					
+						self.expandTable[k] = nil
+						row.panel:Dispose()
+						row.panel = nil
+						row.subrows = nil
+						row.height = TEXT_HEIGHT
+					end,
+					Expand = function(row)
+						if(row.panel)then return end
+					
+						row.panel = Chili.Control:New{
+							parent = row.control,
+							x = 0,
+							y = TEXT_HEIGHT,
+							width = '100%',
+							padding = {10,0,0,0},
+						}
+						local innerExpandTable = self.expandTable[k]
+						if(not innerExpandTable)then
+							innerExpandTable = {}
+							self.expandTable[k] = innerExpandTable
+						end
+						row.subrows = createRows(row.panel, innerExpandTable, function(rows, height)
+							row.panel:SetPos(nil, nil, nil, height)
+							row.height = TEXT_HEIGHT + height
+							row.control:SetPos(nil, nil, nil, row.height)
+						end)
+						row.subrows:SetTable(row.currentValue)
+					end,
 				}
-				top = top + ROW_HEIGHT
+				if(self.expandTable[k])then
+					row:Expand()
+				end
+				top = top + row.height
 				keyMap[k] = row
 				length = length + 1
 				self[length] = row
@@ -323,19 +378,40 @@ do
 			self[i] = nil
 		end
 		self.length = length
+		
+		self.height = top
+		if(self.sizeChangedCallback)then
+			self.sizeChangedCallback(self, top)
+		end
+	end
+	function rowsPrototype:Realign()
+		local top = 0
+		for i = 1, self.length do
+			row = self[i]
+			row.control:SetPos(0, top)
+			top = top + row.height
+		end		
+		
+		self.height = top
+		if(self.sizeChangedCallback)then
+			self.sizeChangedCallback(self, top)
+		end
 	end
 	
 	rowsMetatable = {
 		__index = rowsPrototype
 	}
 	
-	function createRows(wrapper, sizeChangedCallback)
+	function createRows(wrapper, expandTable, sizeChangedCallback)
 		return setmetatable({
+			length = 0,
 			wrapper = wrapper,
 			sizeChangedCallback = sizeChangedCallback,
+			expandTable = expandTable,
 		}, rowsMetatable)
 	end
 end
+local expandedVariablesMap = {} 
 
 local function showCurrentBlackboard(blackboardState)
 	currentBlackboardState = blackboardState
@@ -375,7 +451,7 @@ local function listenerClickOnShowBlackboard()
 		height = '100%',
 	}
 	
-	blackboardWindowState.rows = createRows(blackboardWindowState.contentWrapper)
+	blackboardWindowState.rows = createRows(blackboardWindowState.contentWrapper, expandedVariablesMap)
 	
 	if(currentBlackboardState)then
 		showCurrentBlackboard(currentBlackboardState)
