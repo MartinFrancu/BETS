5781c71a4160077c3559ad9c1ede480deb29d5e6
Martin Francu <martinfrancu@gmail.com>
2017-04-08 17:36:59 +0200

    Changed Utils.Categories to use ProjectManager - just for file location. Right now it assumes you are working with Common project.

--- BtUtils/categories.lua
+++ BtUtils/categories.lua
@@ -7,6 +7,8 @@ local Utils = BtUtils
 local Logger = Utils.Debug.Logger
 local dump = Utils.Debug.dump
 
+
+
 return Utils:Assign("UnitCategories", function()
 	local UnitCategories = {}
 	
@@ -17,10 +19,38 @@ return Utils:Assign("UnitCategories", function()
 	
 	
 	local JSON = Utils.JSON
+	local ProjectManager = Utils.ProjectManager
+	
+	local contentType =  ProjectManager.makeRegularContentType("UnitCategories", "json")
+	UnitCategories.contentType = contentType
 	
 	-- This function load categories from given file and returns them as a table
-	local function loadCategories()
+	local function loadCategories(projectNameIn)
+		local projectName
+		if(projectNameIn ~= nil) then
+			projectName = projectNameIn
+		else
+			projectName = "Common"
+		end
 		local unitCategories = {}
+		
+		local categoryFiles = ProjectManager.listProject(projectName, contentType)
+		Logger.log("categories", "files: ", dump(categoryFiles,2) )
+		for _,catFile in pairs(categoryFiles) do
+			local path = catFile.path
+			local file = io.open(path, "r")
+			if(not file)then
+				Logger.log("categories", "Unable to read category definition file: ", path )
+				return unitCategories
+			end
+			local text = file:read("*all")
+			file:close()
+			local data = JSON:decode(text)
+			-- should I check the category name??
+			--local catName = fileName:sub(UNIT_CATEGORIES_PREFIX:len()+1, fileName:len() - UNIT_CATEGORIES_SUFFIX:len())
+			table.insert(unitCategories, data)
+		end
+		--[[
 		local folderContent = Utils.dirList(UNIT_CATEGORIES_DIRNAME, "*".. UNIT_CATEGORIES_SUFFIX)
 		for _,fileName in pairs(folderContent) do
 			local file = io.open(UNIT_CATEGORIES_DIRNAME .. fileName, "r")
@@ -34,16 +64,39 @@ return Utils:Assign("UnitCategories", function()
 			--local catName = fileName:sub(UNIT_CATEGORIES_PREFIX:len()+1, fileName:len() - UNIT_CATEGORIES_SUFFIX:len())
 			table.insert(unitCategories, data)
 		end
+		]]
 		return unitCategories
 	end
 	
 	UnitCategories.categories = loadCategories()
 	
 	-- This function saves given table into file fo unit categories
-	local function saveCategories()
-		Spring.CreateDir(UNIT_CATEGORIES_DIRNAME)
+	local function saveCategories(projectNameIn)
+		-- DEBUG: use default project name
+		local projectName
+		if(projectNameIn ~= nil) then
+			projectName = projectNameIn
+		else
+			projectName = "Common"
+		end
+		-------------------------------------------
+		
+		Spring.CreateDir(UNIT_CATEGORIES_DIRNAME) -- REMOVE LATER 
 		for _,catData in ipairs(UnitCategories.categories) do
 			local text = JSON:encode(catData, nil, { pretty = true, indent = "\t"})
+			
+			local path,params = ProjectManager.findFile(contentType, projectName, catData.name)
+			Logger.log("categories", "path: " , path, " params: ", params)
+			-- TODO check if i can write in this file
+			local file = io.open(path, "w")
+			if(not file)then
+				Logger.log("categories", "saveCategories: unable to write in file: ", path)
+				return nil
+			end
+			file:write(text)
+			file:close()
+			
+			--[[ OLD save
 			local fileName = UNIT_CATEGORIES_DIRNAME .. UNIT_CATEGORIES_PREFIX .. catData.name .. UNIT_CATEGORIES_SUFFIX 
 			local file = io.open(fileName, "w")
 			if(not file)then
@@ -52,6 +105,7 @@ return Utils:Assign("UnitCategories", function()
 			end
 			file:write(text)
 			file:close()
+			]]
 		end
 		return true
 	end
