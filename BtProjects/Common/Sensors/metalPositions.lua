d779332f5d4628e8b8a51407cd6bf1ea724d8a78
PepeAmpere <machys@gmail.com>
2018-02-21 02:29:16 +0100

    compatibility update for nota and s44 compatible since commit [2c72e79] with nota  [https://trello.com/c/FVpNVJW3/] compatbile since commit [e10787c] with s44 alpha branch

--- BtProjects/Common/Sensors/metalPositions.lua
+++ BtProjects/Common/Sensors/metalPositions.lua
@@ -2,7 +2,7 @@
 GlobalSensorData.mapMetal = GlobalSensorData.mapMetal or (function()
 	local environment = setmetatable({ mapMetal = {} }, { __index = _G })
 	-- utilize the mex_finder functionality in the Noe AI of NOTA
-	VFS.Include("LuaRules/modules/core/ext/mathExt/mathExt.lua", environment, VFS.RAW_FIRST)
+	VFS.Include("modules/core/ext/mathExt/mathExt.lua", environment, VFS.RAW_FIRST)
 	VFS.Include("LuaRules/Configs/noe/modules/tools/mex_finder.lua", environment, VFS.RAW_FIRST)
 	environment.PreparemapMetal()
 	return environment.mapMetal
